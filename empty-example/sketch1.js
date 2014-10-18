2f761cc2de3b226b6fa28851a00a58c0c3c51281
Yuchen Su <psaux0@gmail.com>
2014-10-17 23:20:15 -0400

    Removed Example Files, Add Temperature Line in Sketch1

--- empty-example/sketch1.js
+++ empty-example/sketch1.js
@@ -1,6 +1,30 @@
 var mic, fft;
 var max=0;
 var imax=0;
+var cityArray = {
+  "Boston" : 0,
+  "Chicago" : 0,
+  "NYC": 0,
+}
+
+ var bosCallback = function(data) {
+  var itc = data.query.results.channel.item.condition;
+  console.log(itc.temp);
+  cityArray.Boston = itc.temp;
+  };
+
+ var nyCallback = function(data) {
+  var itc = data.query.results.channel.item.condition;
+  console.log(itc.temp);
+  cityArray.NYC = itc.temp;
+  };
+
+ var chicagoCallback = function(data) {
+  var itc = data.query.results.channel.item.condition;
+  console.log(itc.temp);
+  cityArray.Chicago = itc.temp;
+  };
+
 
 function setup() {
    createCanvas(window.innerWidth,window.innerHeight-50);
@@ -9,18 +33,18 @@ function setup() {
    fft = new p5.FFT();
    fft.setInput(mic);
 
-var query = escape('CAXX0518'),
-    url = "http://query.yahooapis.com/v1/public/yql?q=" + query + "&format=json&callback=?"; 
-
-$.getJSON(url, function(data) {
-  console.log( data );
-});
-
 }
 
 function draw() {
    background(200);
    stroke(255,0,0);
+   /* Draw Temperature Lines here*/
+   textSize(10);
+   for (var key in cityArray) {
+    var yPos = map(cityArray[key],30,120,0,height);
+    text(key + " " + cityArray[key] + "F",0,height-yPos-20,150,100);
+    line(0,height-yPos,width,height-yPos);
+   }
    var spectrum = fft.analyze();
    var ampmax=0;
    for (i = 0; i<spectrum.length; i++) {
