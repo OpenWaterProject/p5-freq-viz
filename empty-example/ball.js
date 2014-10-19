0b1f363a5a1380cd83cbb37c63c7be0cc4de303e
dwblair <donblair@gmail.com>
2014-10-18 21:59:46 -0400

    update

--- empty-example/ball.js
+++ empty-example/ball.js
@@ -3,7 +3,9 @@ var max=0;
 var imax=0;
 
 function setup() {
-   createCanvas(710,400);
+//   createCanvas(710,400);
+createCanvas(window.innerWidth,window.innerHeight-50);
+
    mic = new p5.AudioIn();
    mic.start();
    fft = new p5.FFT();
@@ -18,7 +20,7 @@ function draw() {
    beginShape();
    var ampmax=0;
    for (i = 0; i<spectrum.length; i++) {
-    vertex(i, map(spectrum[i], 0, 255, height, 0) );
+    vertex(i*4, map(spectrum[i], 0, 255, height, 0) );
     if (spectrum[i]>ampmax) {
     //if (i>imax) imax=i;
       ampmax=spectrum[i];
@@ -26,10 +28,11 @@ function draw() {
      }
    }
    endShape();
-   fill(255,0,0,100);
-   //ellipse(imax,map(ampmax,0,255,height,0),map(ampmax,0,255,10,100),map(ampmax,0,255,10,100));
-//ellipse(imax,map(ampmax,0,255,height,0),30,30);
-ellipse(imax,100,30,30);
+   stroke(0,0,255);
+   fill(0,0,255,100);
+//   ellipse(imax,map(ampmax,0,255,height,0),map(ampmax,0,255,height,0),map(ampmax,0,255,height,0));
+ellipse(imax*4,map(ampmax,0,255,height,0),50,50);
+//ellipse(imax,100,30,30);
     //var ss = sort(spectrum);
     //var ssmax = ss[ss.length-1];
     //if (ssmax > max) {
